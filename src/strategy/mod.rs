6e1f36046bf5bd4d3d3841bcc724500f9834e50a
Evgeniy Zuykin <eugenzuy@gmail.com>
2022-07-11 15:34:34 +0700

    1

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -1,6 +1,6 @@
 pub mod util;
 pub mod holder;
-mod module;
+mod behaviour;
 pub mod loot;
 pub mod potential_field;
 
@@ -11,7 +11,7 @@ use crate::model;
 use crate::model::{ActionOrder, Constants, Game, Item, Loot, Unit, UnitOrder, Vec2};
 use crate::model::ActionOrder::Aim;
 use crate::strategy::holder::{get_constants, get_game};
-use crate::strategy::module::{Behaviour, Fighting, MoveToCenterOrLoot, UseHeal};
+use crate::strategy::behaviour::{Behaviour, Fighting, MoveToCenterOrLoot, UseHeal};
 
 pub fn get_order() -> model::Order {
     let game = get_game();
@@ -35,48 +35,9 @@ pub fn get_order() -> model::Order {
             }
         }
         (u.id, order)
-        // let intersected_loot = game.intersecting_loot(u);
-        // let valuable_loot = game.loot.iter().find(|l| item_filter(constants, u, l)).map(|e| e.clone());
-        // let loot_to_pick = intersected_loot.iter().find(|l| item_filter(constants, u, l));
-        //
-        // let target_direction = game.enemy_units().iter()
-        //     .min_by_key(|e| (e.position.distance(&u.position) * 1000.0).ceil() as i64)
-        //     .map(|it| it.position.clone());
-        //
-        // let action = {
-        //     if loot_to_pick.is_some() {
-        //         Some(ActionOrder::Pickup { loot: loot_to_pick.unwrap().id })
-        //     } else if u.ammo[u.weapon.unwrap_or(0) as usize] == 0 {
-        //         None
-        //     } else {
-        //         target_direction.clone().map(|e| Aim {
-        //             shoot: true
-        //         })
-        //     }
-        // };
-        //
-        // let move_target = valuable_loot.map(|e| e.position).unwrap_or(Vec2 {
-        //     x: game.zone.next_center.x,
-        //     y: game.zone.next_center.y,
-        // });
-        // let order = UnitOrder {
-        //     target_velocity: (u.position.clone() - move_target.clone()) * 1000.00,
-        //     action,
-        //     target_direction: u.position.clone() - target_direction.unwrap_or(move_target),
-        // };
-        // (u.id, order)
     }).collect();
 
     model::Order {
         unit_orders: orders,
     }
 }
-
-fn item_filter(constants: &Constants, u: &Unit, l: &Loot) -> bool {
-    match l.item {
-        Item::Weapon { type_index } => { type_index > u.weapon.unwrap_or(-1) && u.ammo[type_index as usize] > 0 as i32 }
-        Item::ShieldPotions { amount } => { u.shield_potions < constants.max_shield_potions_in_inventory }
-        Item::Ammo { weapon_type_index, amount } => { true }
-    }
-}
-
